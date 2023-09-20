be209a558246a774cb2d17eb1373d5cc8eb1c457
Joseph Woodward <joseph.woodward@xeuse.com>
2023-09-20 23:14:09 +0100

    remove need to call setup

--- lua/github-browse/init.lua
+++ lua/github-browse/init.lua
@@ -1,9 +1,9 @@
 ---@class BrowseModule
 local M = {}
 
-function M.setup()
-    vim.api.nvim_create_user_command("GithubBrowse", M.browse,{})
-end
+-- function M.setup()
+--     vim.api.nvim_create_user_command("GithubBrowse", M.browse,{})
+-- end
 
 ---@return string
 M.browse = function()
