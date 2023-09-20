0a67bddbbe9de8d38b97b811f0777df8912dd352
Joseph Woodward <joseph.woodward@xeuse.com>
2023-09-20 23:52:58 +0100

    ensure loaded only once

--- plugin/github-browse.lua
+++ plugin/github-browse.lua
@@ -4,10 +4,10 @@ if vim.fn.has("nvim-0.7.0") == 0 then
 end
 
 -- make sure this file is loaded only once
-if vim.g.loaded_my_awesome_plugin == 1 then
+if vim.g.loaded_github_browse == 1 then
   return
 end
-vim.g.loaded_my_awesome_plugin = 1
+vim.g.loaded_github_browse = 1
 
 -- create any global command that does not depend on user setup
 -- usually it is better to define most commands/mappings in the setup function
